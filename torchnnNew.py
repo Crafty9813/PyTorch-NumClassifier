c15f9f0e7da2ac5c7b7a6e87e0ce95c15050749a
Jonathan Li <152799124+Crafty9813@users.noreply.github.com>
2025-07-02 19:44:22 -0400

    Update torchnnNew.py

--- torchnnNew.py
+++ torchnnNew.py
@@ -61,7 +61,7 @@ if __name__ == "__main__":
     with open("model_state.pt", "rb") as f:
         classifier.load_state_dict(load(f))
 
-        img = Image.open("img_1.jpg")
+        img = Image.open("nums/two.jpg")
         img_tensor = ToTensor()(img).unsqueeze(0).to("mps")
 
         print(torch.argmax(classifier(img_tensor)))
